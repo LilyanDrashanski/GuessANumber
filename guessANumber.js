8294fa1fbcead26cf3df579c39d43e7e3cd93594
LilyanDrashanski <lilyandrashanski@gmail.com>
2023-04-19 18:37:34 +0300

    Added try play again option

--- guessANumber.js
+++ guessANumber.js
@@ -4,18 +4,17 @@ function guessANumber() {
         output: process.stdout
     })
 
-    let computerGuess = Math.floor(Math.random() * 1);
+    let computerGuess = Math.floor(Math.random() * 100);
     let guess;
 
-    let recursiveAsyncReadLine = function () {
-
+    function recursiveAsyncReadLine() {
         readline.question('Guess the number (0-100):', number => {
             guess = Number(number);
 
             if (guess <= 100 && guess >= 0) {
                 if (guess == computerGuess) {
                     console.log('Correct! You guess the number!')
-                    readline.close();
+                    tryAgain()
                 }
                 else if (guess < computerGuess) {
                     console.log("Incorrect! The number is too low!");
@@ -29,10 +28,27 @@ function guessANumber() {
                 console.log("Invalid input! Try again...");
                 recursiveAsyncReadLine();
             }
+
         })
     }
-
     recursiveAsyncReadLine();
+
+    function tryAgain() {
+        readline.question('Do you want to play again?', input => {
+            if (input == "yes" || input == "y") {
+                computerGuess = Math.floor(Math.random() * 100);
+                recursiveAsyncReadLine();
+            }
+            else if (input == "no" || input == "n") {
+                return readline.close();
+            }
+            else {
+                console.log("Please answer with yes or no.");
+                tryAgain();
+            }
+        })
+    }
 }
 
 guessANumber()
+
